4e7bd74c3a332f03349c37ff0e9a9a7a295ea983
Phong Tran <thanhphongtran1703@gmail.com>
2025-12-05 14:58:54 +0000

    pj1

---
+++ project1/index.js
@@ -0,0 +1,24 @@
+console.log("hello");
+console.log("I like pizza");
+
+//window.alert("This is an alert");
+//window.alert("Whats up");
+
+//this is a comment
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "i like pizza";
+
+let x = 100;
+let price = 10.99;
+let gpa = 3.6;
+let age = 22;
+
+let firstName = 'Phong'
+
+let online = true;
+
+console.log(`You are online: ${online}`);
+console.log(`You are ${age} years old`);
+console.log(typeof x);
+console.log(`Your name is ${firstName}`);
